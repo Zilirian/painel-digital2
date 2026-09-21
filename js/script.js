cfff9b663680e34622850794e10984702987631d
Zilirian <sarah.sassal1@gmail.com>
2026-09-21 15:15:54 -0300

    novo projeto

--- js/script.js
+++ js/script.js
@@ -1,58 +1,29 @@
-const horas = document.querySelector('#horas');
-const minutos = document.querySelector('#minutos');
-const segundos = document.querySelector('#segundo'); 
+const pergunta1 = document.querySelector("#pergunta1")
+const pergunta2 = document.querySelector("#pergunta2")
+const pergunta3 = document.querySelector("#pergunta3")
 
-const dia = document.querySelector('#dia');
-const mes = document.querySelector('#mes');
-const ano = document.querySelector('#ano');
+const button1 = document.querySelector("#btn1")
+const button2 = document.querySelector("#btn2")
 
-const dia2 = document.querySelector('#dia2');
-const mes2 = document.querySelector('#mes2');
-const ano2 = document.querySelector('#ano2');
+const resposta1 = document.querySelector("#resposta1")
 
-const mensagem = document.querySelector('#mensagem');
-const bolinha = document.querySelector('#status-bolinha');
+button1.addEventListener("click", () => {
+    let distancia = Number(pergunta1.value)
+    let consumo = Number(pergunta2.value)
+    let preco = Number(pergunta3.value)
 
-function atualizarRelogio() {
-    const agora = new Date();
-
-    let h = agora.getHours();
-    let min = agora.getMinutes();
-    let s = agora.getSeconds();
-
-    let d = agora.getDate();
-    let m = agora.getMonth() + 1;
-    let a = agora.getFullYear();
-
-    if (h < 10) h = '0' + h;
-    if (min < 10) min = '0' + min;
-    if (s < 10) s = '0' + s;
-    if (d < 10) d = '0' + d;
-    if (m < 10) m = '0' + m;
-
-    horas.innerText = h;
-    minutos.innerText = min;
-    segundos.innerText = s;
-
-    dia.innerText = d;
-    mes.innerText = m;
-    ano.innerText = a;
-
-    dia2.innerText = d;
-    mes2.innerText = m;
-    ano2.innerText = a;
-
-    if (h >= 5 && h < 12) {
-        mensagem.innerText = 'Bom dia!';
-    } else if (h >= 12 && h < 18) {
-        mensagem.innerText = 'Boa tarde!';
+    if (isNaN(distancia) || isNaN(consumo) || isNaN(preco) || distancia <= 0 || consumo <= 0 || preco <= 0) {
+        resposta1.textContent = "Por favor, preencha todos os campos com números válidos e maiores que zero!"
     } else {
-        mensagem.innerText = 'Boa noite!';
+        let valor_final = (distancia / consumo) * preco
+        resposta1.textContent = `O custo total de combustível para sua viagem é: R$ ${valor_final.toFixed(2)}`
     }
+});
 
-    bolinha.classList.add('piscar');
-setTimeout(() => bolinha.classList.remove('piscar'), 200);
-}
+button2.addEventListener("click", () => {
+    resposta1.textContent = ""
+    pergunta1.value = ""
+    pergunta2.value = ""
+    pergunta3.value = ""
 
-atualizarRelogio();
-setInterval(atualizarRelogio, 1000);
+});
